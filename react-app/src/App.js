d315f677cf33f6682d2b48880b27fa645f361833
HukovaNastya <hukovanasta@gmail.com>
2023-04-03 12:23:30 +0300

    added new Component into App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Timer from './components/Timer';
+import TimerWIthUseMemo from './components/Timer';
 
 function App() {
   return (
     <div>
       <Timer/>
+      {/* <TimerWIthUseMemo/> */}
     </div>
   );
 }
